e89e6a885fd480e30171ace700825833394ffb17
TeddyArellano <jarellanojr2109@gmail.com>
2025-04-10 13:29:16 -0600

    Agrego logos de IPN en el Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import logoESCOM from '../assets/logoescom.png';
+import logoIPN from '../assets/logoipn.png';
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <ul>
+      <div className="navbar-left">
+        <img src={logoIPN} alt="Logo IPN" className="navbar-logo" />
+        <img src={logoESCOM} alt="Logo ESCOM" className="navbar-logo" />
+      </div>
+      <ul className="navbar-menu">
         <li><Link to="registro" smooth duration={500}>Registro</Link></li>
         <li><Link to="talleres" smooth duration={500}>Talleres</Link></li>
         <li><Link to="ponencias" smooth duration={500}>Ponencias</Link></li>
